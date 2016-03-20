0d9d3bcd3095aaad75ac7992422ddf1a6e6d5212
abel <greenfire@live.co.uk>
2016-03-20 17:19:49 +0000

    setting up the project

---
+++ artificial-evolution/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to artificial-evolution
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ artificial-evolution/src/artificial_evolution/core.clj
@@ -0,0 +1,7 @@
+(ns artificial-evolution.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
