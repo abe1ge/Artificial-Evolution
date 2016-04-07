df826e8139f8912a821084c3dc77c496ff174ed2
abel <greenfire@live.co.uk>
2016-04-07 13:02:25 +0100

    transcription almost working

--- artificial_evolution/src/dna/transcription.clj
+++ artificial_evolution/src/dna/transcription.clj
@@ -25,7 +25,14 @@
 ;      (recur (- end1 1)    ;loop with 2 new arguments
 ;             (conj result (nth DNA end1)))))
 ;  )
-
+(defn all-gene-end [DNA]
+  (loop [end1 (find-geneEnd DNA)
+         result []]
+    (if-not (= end1 -1)
+      result
+      (recur (subs (str DNA) end1)    ;loop with 2 new arguments
+             (conj result (nth DNA end1)))))
+  )
 
 (defn dna-ranM
   "takes RNA and returns
@@ -40,3 +47,34 @@
 
 ;;The messenger RNA needs to be proccesed so the
 ;; only the gene is left
+
+
+(defn afterend [str1 int1]
+  (subs
+    (str str1)
+    int1
+    )
+  )
+(defn reststr [str1]
+  (loop [x (str str1)
+         y (find-geneStart str1)
+         tot 0
+         result []]
+    (if (= y -1)
+      (rest result)
+      (recur (afterend x (+ 1 (find-geneStart x))) ;loop with 2 new arguments
+             (find-geneStart x)
+             (+ tot y)
+             (conj result (vector x)))
+      ))
+  )
+
+(defn genomes []
+  ;(first (first (reststr test-rnaM)))
+  (for [x (reststr test-rnaM)
+        y [find-geneEnd]
+        z [subs]
+        ]
+    [(z (first x) 0 (y (first x)))]
+    )
+  )
